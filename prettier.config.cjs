316db27a005b7a4f19b035267990a6942b5927e7
Stefan Karlsson <iam@stefan-karlsson.se>
2024-03-19 15:27:23 +0100

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  embeddedLanguageFormatting: 'off',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'ignore',
+  jsxSingleQuote: true,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
+
+module.exports = config;
